05dda419da553209c22b4d0c270cfe2215f32dde
dxmasoruiz <145114550+dxmasoruiz@users.noreply.github.com>
2025-01-07 21:09:24 +0200

    cambio en el main

--- multistart/multistart_hooke_seq.c
+++ multistart/multistart_hooke_seq.c
@@ -1,135 +1,3 @@
-/* Nonlinear Optimization using the algorithm of Hooke and Jeeves  */
-/*	12 February 1994	author: Mark G. Johnson 	   */
-
-
-/* Find a point X where the nonlinear function f(X) has a local    */
-/* minimum.  X is an n-vector and f(X) is a scalar.  In mathe-	   */
-/* matical notation  f: R^n -> R^1.  The objective function f()    */
-/* is not required to be continuous.  Nor does f() need to be	   */
-/* differentiable.  The program does not use or require 	   */
-/* derivatives of f().						   */
-
-/* The software user supplies three things: a subroutine that	   */
-/* computes f(X), an initial "starting guess" of the minimum point */
-/* X, and values for the algorithm convergence parameters.  Then   */
-/* the program searches for a local minimum, beginning from the    */
-/* starting guess, using the Direct Search algorithm of Hooke and  */
-/* Jeeves.							   */
-
-/* This C program is adapted from the Algol pseudocode found in    */
-/* "Algorithm 178: Direct Search" by Arthur F. Kaupe Jr., Commun-  */
-/* ications of the ACM, Vol 6. p.313 (June 1963).  It includes the */
-/* improvements suggested by Bell and Pike (CACM v.9, p. 684, Sept */
-/* 1966) and those of Tomlin and Smith, "Remark on Algorithm 178"  */
-/* (CACM v.12).  The original paper, which I don't recommend as    */
-/* highly as the one by A. Kaupe, is:  R. Hooke and T. A. Jeeves,  */
-/* "Direct Search Solution of Numerical and Statistical Problems", */
-/* Journal of the ACM, Vol. 8, April 1961, pp. 212-229. 	   */
-
-/* Calling sequence:						   */
-/*  int hooke(nvars, startpt, endpt, rho, epsilon, itermax)	   */
-/*								   */
-/*     nvars	   {an integer}  This is the number of dimensions  */
-/*		   in the domain of f().  It is the number of	   */
-/*		   coordinates of the starting point (and the	   */
-/*		   minimum point.)				   */
-/*     startpt	   {an array of doubles}  This is the user-	   */
-/*		   supplied guess at the minimum.		   */
-/*     endpt	   {an array of doubles}  This is the location of  */
-/*		   the local minimum, calculated by the program    */
-/*     rho	   {a double}  This is a user-supplied convergence */
-/*		   parameter (more detail below), which should be  */
-/*		   set to a value between 0.0 and 1.0.	Larger	   */
-/*		   values of rho give greater probability of	   */
-/*		   convergence on highly nonlinear functions, at a */
-/*		   cost of more function evaluations.  Smaller	   */
-/*		   values of rho reduces the number of evaluations */
-/*		   (and the program running time), but increases   */
-/*		   the risk of nonconvergence.	See below.	   */
-/*     epsilon	   {a double}  This is the criterion for halting   */
-/*		   the search for a minimum.  When the algorithm   */
-/*		   begins to make less and less progress on each   */
-/*		   iteration, it checks the halting criterion: if  */
-/*		   the stepsize is below epsilon, terminate the    */
-/*		   iteration and return the current best estimate  */
-/*		   of the minimum.  Larger values of epsilon (such */
-/*		   as 1.0e-4) give quicker running time, but a	   */
-/*		   less accurate estimate of the minimum.  Smaller */
-/*		   values of epsilon (such as 1.0e-7) give longer  */
-/*		   running time, but a more accurate estimate of   */
-/*		   the minimum. 				   */
-/*     itermax	   {an integer}  A second, rarely used, halting    */
-/*		   criterion.  If the algorithm uses >= itermax    */
-/*		   iterations, halt.				   */
-
-
-/* The user-supplied objective function f(x,n) should return a C   */
-/* "double".  Its  arguments are  x -- an array of doubles, and    */
-/* n -- an integer.  x is the point at which f(x) should be	   */
-/* evaluated, and n is the number of coordinates of x.	That is,   */
-/* n is the number of coefficients being fitted.		   */
-
-/* rho, the algorithm convergence control			   */
-/*	The algorithm works by taking "steps" from one estimate of */
-/*    a minimum, to another (hopefully better) estimate.  Taking   */
-/*    big steps gets to the minimum more quickly, at the risk of   */
-/*    "stepping right over" an excellent point.  The stepsize is   */
-/*    controlled by a user supplied parameter called rho.  At each */
-/*    iteration, the stepsize is multiplied by rho  (0 < rho < 1), */
-/*    so the stepsize is successively reduced.			   */
-/*	Small values of rho correspond to big stepsize changes,    */
-/*    which make the algorithm run more quickly.  However, there   */
-/*    is a chance (especially with highly nonlinear functions)	   */
-/*    that these big changes will accidentally overlook a	   */
-/*    promising search vector, leading to nonconvergence.	   */
-/*	Large values of rho correspond to small stepsize changes,  */
-/*    which force the algorithm to carefully examine nearby points */
-/*    instead of optimistically forging ahead.	This improves the  */
-/*    probability of convergence.				   */
-/*	The stepsize is reduced until it is equal to (or smaller   */
-/*    than) epsilon.  So the number of iterations performed by	   */
-/*    Hooke-Jeeves is determined by rho and epsilon:		   */
-/*	    rho**(number_of_iterations) = epsilon		   */
-/*	In general it is a good idea to set rho to an aggressively */
-/*    small value like 0.5 (hoping for fast convergence).  Then,   */
-/*    if the user suspects that the reported minimum is incorrect  */
-/*    (or perhaps not accurate enough), the program can be run	   */
-/*    again with a larger value of rho such as 0.85, using the	   */
-/*    result of the first minimization as the starting guess to    */
-/*    begin the second minimization.				   */
-
-/* Normal use: (1) Code your function f() in the C language	   */
-/*	       (2) Install your starting guess {or read it in}	   */
-/*	       (3) Run the program				   */
-/*	       (4) {for the skeptical}: Use the computed minimum   */
-/*		      as the starting point for another run	   */
-
-/* Data Fitting:						   */
-/*	Code your function f() to be the sum of the squares of the */
-/*	errors (differences) between the computed values and the   */
-/*	measured values.  Then minimize f() using Hooke-Jeeves.    */
-/*	EXAMPLE: you have 20 datapoints (ti, yi) and you want to   */
-/*	find A,B,C such that  (A*t*t) + (B*exp(t)) + (C*tan(t))    */
-/*	fits the data as closely as possible.  Then f() is just    */
-/*	f(x) = SUM (measured_y[i] - ((A*t[i]*t[i]) + (B*exp(t[i])) */
-/*				  + (C*tan(t[i]))))^2		   */
-/*	where x[] is a 3-vector consisting of {A, B, C}.	   */
-
-/*								   */
-/*  The author of this software is M.G. Johnson.		   */
-/*  Permission to use, copy, modify, and distribute this software  */
-/*  for any purpose without fee is hereby granted, provided that   */
-/*  this entire notice is included in all copies of any software   */
-/*  which is or includes a copy or modification of this software   */
-/*  and in all copies of the supporting documentation for such	   */
-/*  software.  THIS SOFTWARE IS BEING PROVIDED "AS IS", WITHOUT    */
-/*  ANY EXPRESS OR IMPLIED WARRANTY.  IN PARTICULAR, NEITHER THE   */
-/*  AUTHOR NOR AT&T MAKE ANY REPRESENTATION OR WARRANTY OF ANY	   */
-/*  KIND CONCERNING THE MERCHANTABILITY OF THIS SOFTWARE OR ITS    */
-/*  FITNESS FOR ANY PARTICULAR PURPOSE. 			   */
-/*								   */
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -261,81 +129,89 @@ double get_wtime(void) {
 }
 
 int main(int argc, char *argv[]) {
+    double startpt[MAXVARS], endpt[MAXVARS];
     int itermax = IMAX;
     double rho = RHO_BEGIN;
     double epsilon = EPSMIN;
     int nvars;
-    int ntrials;
-    double best_fx = 1e10;
-    double best_pt[MAXVARS];
-    int best_trial = -1;
-    int best_jj = -1;
+    int trial, ntrials;
+    double fx;
+    int i, jj;
     double t0, t1;
+    double global_best_fx = 1e10;
+    double global_best_pt[MAXVARS];
+    int global_best_trial = -1;
+    int global_best_jj = -1;
 
-    for (int i = 0; i < MAXVARS; i++) best_pt[i] = 0.0;
+    ntrials = 16*1024;    /* number of trials */
+    nvars = 16;           /* number of variables (problem dimension) */
+    srand48(24);   /* srand48(time(0)); */
 
-    ntrials = 16 * 1024; /* number of trials */
-    nvars = 16; /* number of variables (problem dimension) */
-    srand(24); /* Use srand instead of srand48 for portability */
-
-    t0 = omp_get_wtime();
-
-    #pragma omp parallel
+    t0 = get_wtime();
+    
+    #pragma omp parallel private(i, jj, fx, startpt, endpt)
     {
-        double thread_best_fx = 1e10;
-        double thread_best_pt[MAXVARS];
-        int thread_best_trial = -1;
-        int thread_best_jj = -1;
+        double best_fx = 1e10;
+        double best_pt[MAXVARS];
+        int best_trial = -1;
+        int best_jj = -1;
 
-        for (int i = 0; i < MAXVARS; i++) thread_best_pt[i] = 0.0;
+        for (int j = 0; j < MAXVARS; j++) best_pt[j] = 0.0;
 
-        #pragma omp for
-        for (int trial = 0; trial < ntrials; trial++) {
-            double trial_startpt[MAXVARS], trial_endpt[MAXVARS];
-            double fx;
-            int jj;
-
-            unsigned int seed = trial;
-            for (int i = 0; i < nvars; i++) {
-                trial_startpt[i] = -4 + (rand_r(&seed) / (double)RAND_MAX) * 8;
+        #pragma omp for nowait
+        for (trial = 0; trial < ntrials; trial++) {
+            /* starting guess for rosenbrock test function, search space in [-4, 4) */
+            for (i = 0; i < nvars; i++) {
+                startpt[i] = 8.0 * drand48() - 4.0;
             }
 
-            jj = hooke(nvars, trial_startpt, trial_endpt, rho, epsilon, itermax);
-            fx = f(trial_endpt, nvars);
+            jj = hooke(nvars, startpt, endpt, rho, epsilon, itermax);
+            #if DEBUG
+            printf("\n\n\nHOOKE %d USED %d ITERATIONS, AND RETURNED\n", trial, jj);
+            for (i = 0; i < nvars; i++)
+                printf("x[%3d] = %15.7le \n", i, endpt[i]);
+            #endif
 
-            if (fx < thread_best_fx) {
-                thread_best_fx = fx;
-                thread_best_trial = trial;
-                thread_best_jj = jj;
-                for (int i = 0; i < nvars; i++) {
-                    thread_best_pt[i] = trial_endpt[i];
+            fx = f(endpt, nvars);
+            #if DEBUG
+            printf("f(x) = %15.7le\n", fx);
+            #endif
+            
+            if (fx < best_fx) {
+                best_trial = trial;
+                best_jj = jj;
+                best_fx = fx;
+                for (i = 0; i < nvars; i++) {
+                    best_pt[i] = endpt[i];
                 }
             }
         }
 
         #pragma omp critical
         {
-            if (thread_best_fx < best_fx) {
-                best_fx = thread_best_fx;
-                best_trial = thread_best_trial;
-                best_jj = thread_best_jj;
-                for (int i = 0; i < nvars; i++) {
-                    best_pt[i] = thread_best_pt[i];
+            if (best_fx < global_best_fx) {
+                global_best_trial = best_trial;
+                global_best_jj = best_jj;
+                global_best_fx = best_fx;
+                for (i = 0; i < nvars; i++) {
+                    global_best_pt[i] = best_pt[i];
                 }
             }
         }
     }
-
-    t1 = omp_get_wtime();
+    
+    t1 = get_wtime();
 
     printf("\n\nFINAL RESULTS:\n");
-    printf("Elapsed time = %.3lf s\n", t1 - t0);
+    printf("Elapsed time = %.3lf s\n", t1-t0);
     printf("Total number of trials = %d\n", ntrials);
-    printf("Best result at trial %d used %d iterations, and returned\n", best_trial, best_jj);
-    for (int i = 0; i < nvars; i++) {
-        printf("x[%3d] = %15.7le \n", i, best_pt[i]);
+    printf("Total number of function evaluations = %ld\n", funevals);
+    printf("Best result at trial %d used %d iterations, and returned\n", global_best_trial, global_best_jj);
+    for (i = 0; i < nvars; i++) {
+        printf("x[%3d] = %15.7le \n", i, global_best_pt[i]);
     }
-    printf("f(x) = %15.7le\n", best_fx);
+    printf("f(x) = %15.7le\n", global_best_fx);
 
     return 0;
 }
+
